bc05dfab7e67db256758767931a2f89f90b12444
Calvin Doan <doandangkhoa150505@gmail.com>
2025-11-02 23:46:38 +0700

    visualize network test by dijkstra

--- src/visualize_network.py
+++ src/visualize_network.py
@@ -1,14 +1,22 @@
 
 import matplotlib.pyplot as plt
-import networkx as nx
+import networkx as nx 
 from network_builder import create_sample_network
 
+# create graph
 G = create_sample_network()
-pos = nx.spring_layout(G, seed=42)
-edge_labels = nx.get_edge_attributes(G, 'weight')
+
+# create coordinate for nodes
+pos = nx.spring_layout(G, seed=42) # seed=42 --> fixed position 
+edge_labels = nx.get_edge_attributes(G, 'weight') # take edge's weight 
+
+# draw graph 
 nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=800)
-nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels) # adding weight label on each edge
+
+# saving image to file
 plt.title("Sample Network")
 plt.savefig("plots/network_example.png")
 print("Saved plots/network_example.png")
 # plt.show()  # optional to display
+
