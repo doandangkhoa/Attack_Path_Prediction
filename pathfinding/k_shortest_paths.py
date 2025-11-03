3384a9eb8fb19a14047174d78ef8c8d5ead1bcae
Calvin Doan <doandangkhoa150505@gmail.com>
2025-11-03 15:48:08 +0700

    update k shortest paths and test

--- pathfinding/k_shortest_paths.py
+++ pathfinding/k_shortest_paths.py
@@ -3,15 +3,10 @@ from itertools import islice
 
 # Yen's algorithm
 
-def top_k_shortest_paths(graph, source, target, k=3, cutoff=None, weight='weight'):
+def top_k_shortest_paths(G, source, target, k=3, cutoff=None, weight='weight'):
     # k : number of paths, cutoff : maximum nodes per path
     # initial directed graph by networkx from graph
-    G = nx.DiGraph()
     
-    for u in graph:
-        for v, w in graph[u].items():
-            G.add_edge(u, v, weight=w)
-            
     try:
         # generate simple paths by sorting following increased total weight 
         generator = nx.shortest_simple_paths(G, source, target, weight=weight)
--- tests/test_k_shortest_paths.py
+++ tests/test_k_shortest_paths.py
@@ -1,16 +1,9 @@
-
+from src.network_builder import create_sample_network
 from pathfinding.k_shortest_paths import top_k_shortest_paths
 
-graph = {
-    'A': {'B': 2, 'C': 5},
-    'B': {'C': 1, 'D': 4},
-    'C': {'D': 2, 'E': 3},
-    'D': {'F': 1},
-    'E': {'F': 5},
-    'F': {}
-}
 
 if __name__ == "__main__":
+    graph = create_sample_network()
     paths = top_k_shortest_paths(graph, 'A', 'F', k=4)
     print("Top K paths from A to F:")
     for i, path in enumerate(paths, 1):
